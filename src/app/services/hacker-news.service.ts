9778a382a237d9931cdfe72e5756ca93bba116f4
Vincentius <vincentius@bithealth.co.id>
2022-10-25 16:22:47 +0700

    get data for topic list page

--- src/app/services/hacker-news.service.ts
+++ src/app/services/hacker-news.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { concatMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { httpOptions } from './http.util';
 
@@ -10,10 +11,19 @@ export class HackerNewsService {
   constructor(private http: HttpClient) { }
 
   private hackerNewsPrefix = 'https://hacker-news.firebaseio.com/v0/';
+  private hackerNewsSuffix = '.json?print=pretty';
 
-  testGet() {
-    const url = `${this.hackerNewsPrefix}/item/8863.json?print=pretty`;
+  getAllNewsId() {
+    const url = `${this.hackerNewsPrefix}askstories${this.hackerNewsSuffix}`;
 
-    return this.http.get<any>(url, httpOptions);
+    return this.http
+      .get<any>(url, httpOptions);
+      // .pipe(concatMap(val => this.http.get(`${this.hackerNewsPrefix}item/${val}${this.hackerNewsSuffix}`)));
+  }
+
+  getNewsDetailById(id: number) {
+    const url = `${this.hackerNewsPrefix}item/${id}${this.hackerNewsSuffix}`;
+    return this.http
+      .get<any>(url, httpOptions);
   }
 }
--- src/app/topic-list/topic-list.component.ts
+++ src/app/topic-list/topic-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { forkJoin, zip } from 'rxjs';
 import { HackerNewsService } from '../services/hacker-news.service';
 
 @Component({
@@ -9,16 +10,27 @@ import { HackerNewsService } from '../services/hacker-news.service';
 export class TopicListComponent implements OnInit {
 
   constructor(private hackerNewsService: HackerNewsService) { }
-
+  public questionsList = [];
   ngOnInit() {
-    this.testGet();
+    this.getNews();
   }
 
-  testGet() {
-    this.hackerNewsService.testGet().subscribe(res => {
-      console.log(res);
-    }, err => {
-      console.log(err);
+  getNews() {
+    this.hackerNewsService.getAllNewsId().subscribe(res => {
+      const questions = res.slice(0, 20);
+
+      Promise.all(
+        questions.map(questionId => {
+          this.hackerNewsService.getNewsDetailById(questionId).toPromise()
+            .then(val => {
+              this.questionsList.push(val);
+            }).catch(err1 => {
+              console.log(err1);
+            });
+        })
+      );
+    }, err2 => {
+      console.log(err2);
     }
     );
   }
